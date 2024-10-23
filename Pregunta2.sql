d38d28ca63bcc81dd279f9536d8735385f5f9354
GinoRL <griccilopez@gmail.com>
2024-10-23 11:17:51 +0200

    Delete Pregunta2.sql

--- Pregunta2.sql
+++
@@ -1,9 +0,0 @@
--- ¿Cuál es el producto mmás vendido? ProductID, Name, ProductCategoryID
-
-SELECT TOP 10 ProductCategoryID, 
-    MAX(Name) AS Nombre_Producto,
-    COUNT(*) AS Ventas_Totales
-FROM SalesLT.Product
-GROUP BY ProductCategoryID
-ORDER BY Ventas_Totales DESC;
-
