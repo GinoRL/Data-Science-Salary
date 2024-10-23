0f02666d20ac188ac0fd1dde506528e073b95b8d
GinoRL <griccilopez@gmail.com>
2024-10-23 11:18:28 +0200

    Delete Pregunta6.sql

--- Pregunta6.sql
+++
@@ -1,6 +0,0 @@
---	¿Quién ha vendido más? SalesLT.Customer SalesPerson
-
-SELECT SalesPerson, COUNT(*) AS Vendedor_del_siglo
-FROM SalesLT.Customer
-GROUP BY SalesPerson
-ORDER BY Vendedor_del_siglo DESC; 
