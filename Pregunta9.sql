928c83f24752b1acbc8ea173075352e64f28d51a
GinoRL <griccilopez@gmail.com>
2024-10-23 11:18:51 +0200

    Delete Pregunta9.sql

--- Pregunta9.sql
+++
@@ -1,7 +0,0 @@
---	¿Description más larga? SalesLT.ProductDescription Description
-SELECT TOP 10
-    Description,
-    (LEN(LTRIM(RTRIM(Description))) - LEN(REPLACE(Description, ' ', '')) + 1) AS Numero_palabras
-FROM SalesLT.ProductDescription
-WHERE LEN(LTRIM(RTRIM(Description))) > 0
-ORDER BY Numero_palabras DESC;
