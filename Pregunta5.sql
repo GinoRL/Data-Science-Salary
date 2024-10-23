863d4da6aaea3c3486f9577b1b7792b8177d3d97
GinoRL <griccilopez@gmail.com>
2024-10-23 11:18:20 +0200

    Delete Pregunta5.sql

--- Pregunta5.sql
+++
@@ -1,6 +0,0 @@
--- 	¿En qué país hay más demanda? SalesLT.Address CountryRegion
-
-SELECT TOP 5 CountryRegion, COUNT(*) Numero_ventas
-FROM SalesLT.Address
-GROUP BY CountryRegion
-ORDER BY Numero_ventas DESC;
